45ecb387faa1deb8172b39c9bc8b56c2d8edfa42
Daniele Maselli <danymase@gmail.com>
2024-01-25 16:01:38 +0100

    about

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import AddContent from "./pages/AddContent";
+import About from "./pages/About";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/add" element={<AddContent />} />
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AuthContextProvider from "./contexts/AuthContext.jsx";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <BrowserRouter>
+    <BrowserRouter>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
